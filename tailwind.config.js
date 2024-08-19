95e4bb5a9214506f7b878267065883f037366523
SHAKIL AHMED BILLAL <itzxhakil@gmail.com>
2024-08-19 19:17:53 +0600

    tea house tailwind css web site header part and feature container section and mobile view responsive section all complete

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      serif: ['Manrope', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
